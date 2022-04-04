b48db7891b479943adcb19d4dd06e5c551c99248
lamtruong <lamtruongle0503@gmail.com>
2022-04-04 14:27:33 +0700

    asdad

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,16 @@
 class CommentsController < ApplicationController
-
-    def new 
-        @comment = Comment.new
-    end
-
-    
-
+    def new
+        @comment = current_user.comments.new(commentable: @commentable)
+      end
     def create
-        # @commentable = find_commentable
-        # @comment = @commentable.comments.create(comment_params) 
-        # @comment.user = current_user
-        # @comment = @commentable.comments.build(comment_params)
-        @micropost = Micropost.find(params[:micropost_id])
-        @comment = @micropost.comments.create(comment_params) 
-        @comment.micropost = @micropost
+        @commentable = find_commentable
+        @comment = @commentable.comments.create(comment_params) 
         @comment.user = current_user
+        @comment = @commentable.comments.build(comment_params)
+        # @micropost = Micropost.find(params[:micropost_id])
+        # @comment = @micropost.comments.create(comment_params) 
+        # @comment.micropost = @micropost
+        # @comment.user = current_user
         
         
         if @comment.save
